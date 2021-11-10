a8f7caf3c61ef01c260a06c1a9df2186473adba0
Chen Jenfu <dht98010@gmail.com>
2021-11-10 16:50:12 +0800

    1.0.5

--- C++/0temp3.cpp
+++ C++/0temp3.cpp
@@ -2,5 +2,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    printf("Hello World");
+    int grade = 50;
+    if(grade<60)
+        printf("A");
+    else
+        if(grade>70)
+            printf("B");
+        else
+            printf("C");
+            printf("D");
+    printf("E");
 }
