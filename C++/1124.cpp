b1c10dc30c8b6eece5466d05347ba55cf7bf27a7
Chen Jenfu <dht98010@gmail.com>
2021-11-24 10:26:18 +0800

    1.0.7

---
+++ C++/1124.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main() {
+    while (1) {
+        int num;
+        cin >> num;
+        if (num <= 0) {
+            break;
+        }
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                printf("%d*%d=%d ", i, j, i * j);
+            }
+            printf("\n");
+        }
+    }
+}
