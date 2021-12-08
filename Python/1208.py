2ccf543989faf8fe8802a6680bfe9a32ca3e6558
Chen Jenfu <dht98010@gmail.com>
2021-12-08 16:51:43 +0800

    1.1.1

---
+++ Python/1208.py
@@ -0,0 +1,19 @@
+def transfer(num, base):
+    if num//base == 0:  # 取商
+        num = str(num)
+        return num
+    else:
+        x = num % base
+        num = num//base
+        s = str(x)
+        return transfer(num, B)+s
+
+
+while(1):
+    data = input()
+    newdata = data.split()
+    N = int(newdata[0])
+    B = int(newdata[1])
+    if(N < 0 or B > 10 or B < 1):
+        break
+    print(transfer(N, B))
