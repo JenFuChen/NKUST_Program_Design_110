bdc3e32f9c715a398f80eb91fff775228a5ab021
Chen Jenfu <dht98010@gmail.com>
2022-02-19 17:49:19 +0800

    2.0.1

---
+++ C++/ZeroJudge/c659.cpp
@@ -0,0 +1,22 @@
+// C659 Finish
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char arr[100][100];
+    int cnt = 0;
+    while (scanf("%s", arr[cnt])) {
+        if (getchar() == '\n')
+            break;
+        cnt += 1;
+    }
+    for (int x = 1; x <= cnt; x++) {
+        if (x != cnt) {
+            printf("%s %s ", arr[x], arr[0]);
+        } else {
+            printf("%s", arr[x]);
+        }
+    }
+    return 0;
+}
