517941137b29c3fbb0ddbee5fb18f107415e76d5
Chen Jenfu <dht98010@gmail.com>
2022-03-30 01:36:17 +0800

    0323 Py Hw Finished

--- Python/110-2/0323.py
+++ Python/110-2/0323.py
@@ -20,17 +20,22 @@ contentVar.set("開始遊戲")
 
 # 設定各種變數
 player = 1
-checkWin = 0
 counter = 0
 side = 3
 buttonList = list()
-Player1 = []
-Player2 = []
 IndexList = []
-# 贏的情況
-rowWin = []
-colWin = []
-slashWin = 0
+cause = 0
+start = 0
+
+
+def show():
+    global side
+    n = side
+    print("--Index--")
+    for i in range(n):
+        for j in range(n):
+            print(IndexList[i][j], end=" \t")
+        print()
 
 
 def layout(n):
@@ -47,41 +52,23 @@ def layout(n):
         Index.grid(row=row+1, column=col)
 
 
-layout(side)
-
-
-def show():
-    global side, Player1, Player2
-    n = side
-    print("--1--")
-    for i in range(n):
-        for j in range(n):
-            print(Player1[i][j], end=" ")
-        print()
-    print("--2--")
-    for i in range(n):
-        for j in range(n):
-            print(Player2[i][j], end=" ")
-        print()
-    print("--3--")
-    for i in range(n):
-        for j in range(n):
-            print(IndexList[i][j], end=" ")
-        print()
-
-
 def createPlay(n):
+    global cause
     print("[ Reset The Game] ")
-    global Player1, Player2, IndexList
-    Player1 = []
-    Player2 = []
+    if(cause == 1):
+        print("Start Game")
+    elif(cause == 2):
+        print("Cause Win Judge")
+    elif(cause == 3):
+        print("Cause Counter")
+    elif(cause == 4):
+        print("Cause Token")
+    global IndexList
     IndexList = []
     for i in range(n):
         temp = []
         for j in range(n):
             temp.append(0)
-        Player1.append(temp)
-        Player2.append(temp)
         IndexList.append(temp)
 
     for i in range(side*side):
@@ -89,25 +76,29 @@ def createPlay(n):
     show()
 
 
-createPlay(3)
+layout(side)
+createPlay(side)
+show()
 
 
 def FillIn(token, x, y, z):
-    global player, checkWin, counter, Player1, Player2, buttonList, side
+    global player, counter, buttonList, side, start, cause
+    if(start == 0):
+        createPlay(side)
+        start = 1
     print("(", x, ",", y, "), Index = ", z, "Player is ", player)
-
     if(IndexList[x][y] == 0):
         if (player == 1):
-            print("Player1")
+            print("Filled Player1")
             IndexList[x][y] = 1
             buttonList[z].set('X')
-            Player1[x][y] = z + 1
-            player = 2
-        if (player == 2):
-            print("Player2")
-            IndexList[x][y] = 1
+        elif (player == 2):
+            print("Filled Player2")
+            IndexList[x][y] = -1
             buttonList[z].set('O')
-            Player2[x][y] = z + 1
+        if(player == 1):
+            player = 2
+        else:
             player = 1
         content = "換玩家" + str(player)
         contentVar.set(content)
@@ -117,80 +108,56 @@ def FillIn(token, x, y, z):
             contentVar.set("請按別的地方！")
         elif(token == 1):
             contentVar.set("Restart")
-    show()
-    winJudge(Player1, 1)
-    winJudge(Player2, 2)
 
-    if(checkWin == 0 and counter == 9) or (checkWin == 1 or counter == 9 or checkWin == 2 or token == 1):
+    if(counter == side*side or token == 1):
         counter = 0
-        checkWin = 0
         player = 1
-        contentVar.set("【 Tie 】")
+        if(token == 1):
+            contentVar.set("Restart")
+            cause = 4
+        else:
+            cause = 3
+            contentVar.set("【 Tie 】")
         createPlay(side)
 
-
-def winCondition():
-    temp = 0
-    global rowWin, colWin, side, slashWin, checkWin
-    rowWin = []
-    colWin = []
-    slashWin = 0
-    for i in range(1, side+1):
-        temp += i
-    for i in range(side):
-        rowWin.append(temp)
-        temp += side*side
-    temp = 0
-    leftCorner = side*side - side + 1
-    for i in range(1, leftCorner+1, side):
-        temp += i
-    for i in range(side):
-        colWin.append(temp)
-        temp += side
-    temp = 0
-    for i in range(1, side*side, (side+1)):
-        temp += i
-    slashWin = temp
-    print("rolWin ", rowWin, ", colWin ", colWin, ", slashWin ", slashWin)
+    show()
+    winJudge(IndexList)
 
 
-def winJudge(Player, num):
-    print(Player)
-    winCondition()
-    global checkWin, side, rowWin, colWin, slashWin
+def winJudge(list):
+    global side, player, cause, counter
     sumRow, sumCol, sumSlash_L, sumSlash_R = 0, 0, 0, 0
-    for i in range(3):
-        if(Player2[i][0] + Player2[i][1] + Player2[i][2]) == rowWin[i]:
-            contentVar.set("Player 2 Win!")
-            checkWin = 2
-        if(Player2[0][i] + Player2[1][i] + Player2[2][i]) == colWin[i]:
-            contentVar.set("Player 2 Win!")
-            checkWin = 2
-    if(Player2[0][0] + Player2[1][1] + Player2[2][2]) == 15:
-        contentVar.set("Player 2 Win!")
-        checkWin = 2
-    if(Player2[0][2] + Player2[1][1] + Player2[2][0]) == 15:
-        contentVar.set("Player 2 Win!")
-        checkWin = 2
+    P1_Win = side
+    P2_Win = 0 - side
     for i in range(side):
         for j in range(side):
-            sumRow = sumRow + Player[i][j]  # 橫排相加
-            sumCol = sumCol + Player[j][i]  # 直排相加
-            sumSlash_R = sumSlash_R + Player[j][j]  # 左斜 \
-            sumSlash_L = sumSlash_L + Player[j][side-j-1]  # 右斜 /
-        print(" R: ", sumRow, ",C: ", sumCol, ",S_L:",
-              sumSlash_L, ",S_R:", sumSlash_R)
-        if(sumRow == rowWin[i] or sumCol == colWin[i] or sumSlash_L == slashWin or sumSlash_R == slashWin):
-            content = "Player " + str(num) + " Win!"
+            sumRow = sumRow + list[i][j]  # 橫排相加
+            sumCol = sumCol + list[j][i]  # 直排相加
+            sumSlash_R = sumSlash_R + list[j][j]  # 左斜 \
+            sumSlash_L = sumSlash_L + list[j][side-j-1]  # 右斜 /
+        # print("R:", sumRow, "\t C:", sumCol, "\t S_R \ :",
+        #       sumSlash_R, "\t S_L \ :", sumSlash_L)
+        if(sumRow == P1_Win or sumCol == P1_Win or sumSlash_L == P1_Win or sumSlash_R == P1_Win) and (player == 2):
+            content = "Player " + str(1) + " Win!"
             contentVar.set(content)
-            checkWin = num
+            cause = 2
+            counter = 0
+            createPlay(side)
+        elif(sumRow == P2_Win or sumCol == P2_Win or sumSlash_L == P2_Win or sumSlash_R == P2_Win) and (player == 1):
+            content = "Player " + str(2) + " Win!"
+            contentVar.set(content)
+            cause = 2
+            counter = 0
             createPlay(side)
         sumRow, sumCol, sumSlash_L, sumSlash_R = 0, 0, 0, 0
 
 
 def expand():
     global side
-    side += 1
+    if(side >= 7):
+        contentVar.set("無法顯示過大畫面")
+    else:
+        side += 1
     layout(side)
     createPlay(side)
 
--- Python/110-2/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-stringTest = '''hello jer\nh whnsn uiwshnef jnsxc jknjisdnfgviju sdnjkdsxni lksdn juiondjksfgn jkxnckgjfn jxncvkln kashn uinsfkldg nm'''
-num = stringTest.split('\n')
-print(num)
-# newNum = []
-# for i in range(len(num)):
-#     newNum.append(num[i])
-# print(newNum)
-# print(num)
