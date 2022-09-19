7684ffeffddafc3edb56a7dad1ea29576ea9f15a
Chen Jenfu <dht98010@gmail.com>
2022-09-19 18:03:54 +0800

    5.1.1

--- C/111-1/0919_1.cpp
+++ C/111-1/0919_1.cpp
@@ -1,33 +1,59 @@
 #include <math.h>
 #include <stdio.h>
-#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+/*======<格式化輸出>=======*/
+void is_int(int x) {
+    printf("%d\n", x);
+}
+void is_char(char x) {
+    printf("%c\n", x);
+}
+void is_string(char* x) {
+    printf("%s\n", x);
+}
+void is_double(double x) {
+    printf("%f", x);
+}
+void is_float(float x) {
+    printf("%.2f\n", x);
+}
+#define print(X) \
+    _Generic((X),int:is_int,\
+    char:is_char,\
+    char *:is_string,\
+    double:is_double,\
+    default:is_float\
+    )(X)
 // std::cout << "----2\n";
 
 // 尚未完成
 int reverse(int num) {
-    int result = 0, cnt = 0;
+    int result = 0, cnt = 0, power = 0;
+    int old_num = num;
+    while (old_num > 0) {
+        cnt += 1;
+        old_num /= 10;
+    }
+    power = pow(10, cnt - 1);
     while (num > 0) {
-        result = num % 10 + pow(10, cnt) * result;
+        result += power * (num % 10);
         num /= 10;
-        cnt++;
+        power /= 10;
     }
     return result;
 }
 int main() {
-    while (1) {
-        long long int num1 = 0, num2 = 0, sub = 0, reverseNum = 0;
-        double answer = 0;
-        scanf("%lld %lld", &num1, &num2);
+    int num1 = 0, num2 = 0, sub = 0, reverseNum = 0;
+    while (scanf("%d %d", &num1, &num2) != EOF) {
         if (num1 == -1 && num2 == -1) {
             break;
         }
         sub = num1 - num2;
         if (sub < 0) {
-            std::cout << "Error" << std::endl;
+            printf("Error\n");
         } else {
-            reverseNum = reverse(sub);
-            answer = sqrt(reverseNum);
-            printf("%.2f\n", answer);
+            is_float(sqrt(reverse(sub)));
         }
     }
     return 0;
