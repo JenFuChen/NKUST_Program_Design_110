e34e0a0ef533d4bf20bc2a1504c1da7dca0fb1f2
Chen Jenfu <dht98010@gmail.com>
2022-10-05 18:16:16 +0800

    Finished HW7 and update some code

--- C/111-1/1003_7.cpp
+++ C/111-1/1003_7.cpp
@@ -1,24 +1,130 @@
 #include <stdio.h>
-// Haven't done
-// This HW is about count the score of each player's card 
+#include <iostream>
+// Done: 11 % similar to others'
+// This HW is about count the score of each player's card
 
 int main() {
-    int playerNum = 0;        // Player Number
-    int playerCard[5] = {0};  // Store Player's card (only 5 card in once)
-    int counter[13] = {0};    // Counter of Each Card Num
-    int suit[4] = {0};        // Store the Suit
-    int checker = 0;
-    for (int i = 0; i < 5; i++) {
-        if ((playerCard[i] / 13) == 0) {
-            suit[0] += 1;
-        } else if ((playerCard[i] / 13) == 1) {
-            suit[1] += 1;
-        } else if ((playerCard[i] / 13) == 2) {
-            suit[2] += 1;
-        } else if ((playerCard[i] / 13) == 3) {
-            suit[3] += 1;
+    while (1) {
+        int playerNum = 0;  // How many player
+        scanf("%d", &playerNum);
+        if (playerNum <= 0) {
+            break;
+        }
+        while (playerNum--) {
+            getchar();
+            int playerCard[5] = {
+                0};                 // Store player's card (only 5 card in once)
+            int counter[13] = {0};  // Counter of each card num
+            int suit[4] = {0};      // Store the card suit num
+            char Suit[4][99] = {{"Spade"}, {"Heart"}, {"Diamond"}, {"Club"}};
+            int cardPoint[5] = {0};
+            int checker = -1, temp = 0;
+            // input user card
+            for (int i = 0; i < 5; i++) {
+                scanf("%d", &playerCard[i]);
+            }
+            // detect the suit of each card
+            for (int i = 0; i < 5; i++) {
+                cardPoint[i] =
+                    playerCard[i] % 13 == 0 ? 13 : playerCard[i] % 13;
+                counter[(playerCard[i] - 1) % 13] += 1;
+                if (((playerCard[i] - 1) / 13) == 0) {  // Spade
+                    suit[0] += 1;
+                } else if (((playerCard[i] - 1) / 13) == 1) {  // Heart
+                    suit[1] += 1;
+                } else if (((playerCard[i] - 1) / 13) == 2) {  // Diamond
+                    suit[2] += 1;
+                } else if (((playerCard[i] - 1) / 13) == 3) {  // Club
+                    suit[3] += 1;
+                }
+            }
+            // Sort the point
+            for (int i = 0; i < 5; i++) {
+                for (int j = i + 1; j < 5; j++) {
+                    if (cardPoint[i] > cardPoint[j]) {
+                        temp = cardPoint[i];
+                        cardPoint[i] = cardPoint[j];
+                        cardPoint[j] = temp;
+                    }
+                }
+            }
+            temp = 0;
+            // Output the result to view
+            // printf("|黑桃 %d| 紅心 %d| 方塊 %d| 梅花 %d|\n", suit[0],
+            // suit[1], suit[2],
+            //        suit[3]);
+
+            // for (int i = 0; i < 13; i++) {
+            //     printf("%d ", counter[i]);
+            // }
+            // printf("\n");
+            int special[5] = {1, 10, 11, 12, 13};
+            for (int i = 0; i < 5; i++) {
+                if (cardPoint[i] == special[i]) {
+                    temp += 1;
+                }
+            }
+            if (temp == 5) {
+                checker = 3;
+            }
+            temp = 0;
+            for (int i = 0; i < 4; i++) {
+                if (suit[i] == 5) {
+                    for (int j = 0; j < 5; j++) {
+                        if ((cardPoint[j] == cardPoint[j + 1] - 1) && j != 4) {
+                            temp += 1;
+                        }
+                    }
+                }
+            }
+            if (temp == 4) {
+                checker = 0;  // 同花順
+            } else {
+                temp = 0;
+                for (int j = 0; j < 5; j++) {
+                    if (j != 4 && (cardPoint[j] == cardPoint[j + 1] - 1)) {
+                        temp += 1;
+                    }
+                }
+                if (temp == 4) {
+                    checker = 3;  //順子
+                }
+            }
+            if (checker == -1) {
+                for (int i = 0; i < 13; i++) {
+                    if (counter[i] == 4) {  // 四條
+                        checker = 1;
+                    } else if (counter[i] == 3) {
+                        for (int j = i + 1; j < 13; j++) {
+                            if (counter[j] == 2) {
+                                checker = 2;  // 葫蘆
+                                break;
+                            } else {
+                                checker = 4;  //三條
+                            }
+                        }
+                    } else if (counter[i] == 2 && checker == -1) {
+                        for (int j = i + 1; j < 13; j++) {
+                            if (counter[j] == 3) {
+                                checker = 2;
+                                break;
+                            } else if (counter[j] == 2) {
+                                checker = 5;  // 兩對
+                                break;
+                            } else {
+                                checker = 6;  //一對
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (checker == -1) {
+                checker = 7;
+            }
+            printf("%d\n", 7 - checker);
         }
     }
-    
+
     return 0;
 }
--- C/程式驗證/User_Code.cpp
+++ C/程式驗證/User_Code.cpp
@@ -7,66 +7,131 @@ freopen("user_output.txt", "w", stdout);
 #include <stdlib.h>
 #include <string.h>
 
-int reverse(int N) {
-    int reverseNum = 0;
-    while (N > 0) {
-        reverseNum = (N % 10) + reverseNum * 10;
-        N = N / 10;
-    }
-    return reverseNum;
-}
 int main() {
-    freopen("input.txt", "r", stdin);
+    freopen("data_input.txt", "r", stdin);
     freopen("user_output.txt", "w", stdout);
     /* ========== Your Code ========== */
     while (1) {
-        int input = 0;
-        int reverseNum = 0;
-        int judge = 0;    // 1: prime, 2: emirp, 0: None
-        int checker = 0;  // confirm if there is any num can be divided
-        scanf("%d", &input);
-        if (input <= 1) {
+        int playerNum = 0;  // How many player
+        scanf("%d", &playerNum);
+        if (playerNum <= 0) {
             break;
         }
-        reverseNum = reverse(input);
-        // printf("%d\n", reverseNum);
-
-        for (int i = 2; i < input; i++) {
-            if (input % i == 0) {
-                judge = 0;
-                break;
-            } else {
-                judge = 1;
+        while (playerNum--) {
+            getchar();
+            int playerCard[5] = {
+                0};                 // Store player's card (only 5 card in once)
+            int counter[13] = {0};  // Counter of each card num
+            int suit[4] = {0};      // Store the card suit num
+            char Suit[4][99] = {{"Spade"}, {"Heart"}, {"Diamond"}, {"Club"}};
+            int cardPoint[5] = {0};
+            int checker = -1, temp = 0;
+            // input user card
+            for (int i = 0; i < 5; i++) {
+                scanf("%d", &playerCard[i]);
             }
-        }
-        if (judge == 1 && input != reverseNum) {
-            for (int i = 2; i < reverseNum; i++) {
-                if (reverseNum % i == 0) {
-                    judge = 1;
-                    break;
-                } else {
-                    judge = 2;
+            // detect the suit of each card
+            for (int i = 0; i < 5; i++) {
+                cardPoint[i] =
+                    playerCard[i] % 13 == 0 ? 13 : playerCard[i] % 13;
+                counter[(playerCard[i] - 1) % 13] += 1;
+                if (((playerCard[i] - 1) / 13) == 0) {  // Spade
+                    suit[0] += 1;
+                } else if (((playerCard[i] - 1) / 13) == 1) {  // Heart
+                    suit[1] += 1;
+                } else if (((playerCard[i] - 1) / 13) == 2) {  // Diamond
+                    suit[2] += 1;
+                } else if (((playerCard[i] - 1) / 13) == 3) {  // Club
+                    suit[3] += 1;
                 }
             }
-        }
-        if (input == 2) {
-            judge = 1;
-        }
-        switch (judge) {
-            case 0:
-                printf("not prime\n");
-                break;
-            case 1:
-                printf("prime\n");
-                break;
-            case 2:
-                printf("emirp\n");
-                break;
-            default:
-                break;
+            // Sort the point
+            for (int i = 0; i < 5; i++) {
+                for (int j = i + 1; j < 5; j++) {
+                    if (cardPoint[i] > cardPoint[j]) {
+                        temp = cardPoint[i];
+                        cardPoint[i] = cardPoint[j];
+                        cardPoint[j] = temp;
+                    }
+                }
+            }
+            temp = 0;
+            // Output the result to view
+            // printf("|黑桃 %d| 紅心 %d| 方塊 %d| 梅花 %d|\n", suit[0],
+            // suit[1], suit[2],
+            //        suit[3]);
+
+            // for (int i = 0; i < 13; i++) {
+            //     printf("%d ", counter[i]);
+            // }
+            // printf("\n");
+            int special[5] = {1, 10, 11, 12, 13};
+            for (int i = 0; i < 5; i++) {
+                if (cardPoint[i] == special[i]) {
+                    temp += 1;
+                }
+            }
+            if (temp == 5) {
+                checker = 3;
+            }
+            temp = 0;
+            for (int i = 0; i < 4; i++) {
+                if (suit[i] == 5) {
+                    for (int j = 0; j < 5; j++) {
+                        if ((cardPoint[j] == cardPoint[j + 1] - 1) && j != 4) {
+                            temp += 1;
+                        }
+                    }
+                }
+            }
+            if (temp == 4) {
+                checker = 0;  // 同花順
+            } else {
+                temp = 0;
+                for (int j = 0; j < 5; j++) {
+                    if (j != 4 && (cardPoint[j] == cardPoint[j + 1] - 1)) {
+                        temp += 1;
+                    }
+                }
+                if (temp == 4) {
+                    checker = 3;  //順子
+                }
+            }
+            if (checker == -1) {
+                for (int i = 0; i < 13; i++) {
+                    if (counter[i] == 4) {  // 四條
+                        checker = 1;
+                    } else if (counter[i] == 3) {
+                        for (int j = i + 1; j < 13; j++) {
+                            if (counter[j] == 2) {
+                                checker = 2;  // 葫蘆
+                                break;
+                            } else {
+                                checker = 4;  //三條
+                            }
+                        }
+                    } else if (counter[i] == 2 && checker == -1) {
+                        for (int j = i + 1; j < 13; j++) {
+                            if (counter[j] == 3) {
+                                checker = 2;
+                                break;
+                            } else if (counter[j] == 2) {
+                                checker = 5;  // 兩對
+                                break;
+                            } else {
+                                checker = 6;  //一對
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (checker == -1) {
+                checker = 7;  // 雜牌
+            }
+            printf("%d\n", 7 - checker);
         }
     }
-
     /* ========== End ========== */
     return 0;
 }
