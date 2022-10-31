dabf9118bf7087574e66491a120bd6234d1c4de6
Chen Jenfu <dht98010@gmail.com>
2022-10-31 19:04:33 +0800

    new upgrade

--- Python/0temp.py
+++
@@ -1,5 +0,0 @@
-nums = [1,2,3,4,5,6,1,2,3]
-cnt = 1
-str = "()"
-for i in str:
-    print(i)
--- Python/110-2/0330.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-root = Tk()
-# Tk 內的 Image 只能引入 PNG
-logo = PhotoImage(file="Python/IMG_0730.png")
-Label(root, image=logo).pack(side="right")
-
-Label(root, text="KUAS").pack(side="left")
-root.mainloop()
